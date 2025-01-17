92626eba741cf03bee1dd0bb0511d993e03851f8
Azrril <azzrildiki@gmail.com>
2025-01-17 11:09:07 +0700

    Add files via upload

---
+++ acak.php
@@ -0,0 +1,9 @@
+<?php
+
+// logikanya seperti main kartu di acak lalu yang paling atas akan di tamoilkana
+$data = ["ani","budi","charli","doni","eli","fitra"];
+shuffle($data);
+
+echo $data[0];
+
+?>
